1d56dd47c88237a5cff5b78af966b2148c9f1246
Akhil <akhilparaka@gmail.com>
2024-11-03 13:53:13 +0530

    routing scaffolding

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -4,20 +4,37 @@ import Azuki from "../../assets/Azuki Logo White.svg";
 // import { useAppKit } from "@reown/appkit/react";
 // import { useAccount } from "wagmi";
 import { useEffect, useState } from "react";
+import { useNavigate } from "@remix-run/react";
 
 const navbarContents = ["About", "Akhil", "Lore", "More", "Social"];
+
+const navbarRoutes = {
+  About: "/about",
+  Akhil: "/akhil",
+  Lore: "/lore",
+  More: "/more",
+  Social: "/social",
+};
 
 const Header = () => {
   // const { open } = useAppKit();
   // const { address } = useAccount();
   const [isHydrated, setIsHydrated] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setIsHydrated(true);
   }, []);
 
   const handleConnectClick = () => {
-    open();
+    // open();
+  };
+
+  const handleNavigate = (item) => {
+    const route = navbarRoutes[item];
+    if (route) {
+      navigate(route);
+    }
   };
 
   return (
@@ -31,10 +48,16 @@ const Header = () => {
             <div className="connect_wallet"></div>
             <div className="navbar_links">
               {navbarContents.map((item) => (
-                <p key={item}>{item}</p>
+                <p
+                  key={item}
+                  onClick={() => handleNavigate(item)}
+                  style={{ cursor: "pointer" }}
+                >
+                  {item}
+                </p>
               ))}
             </div>
-            <div className="connect_wallet" /*onClick={handleConnectClick}*/>
+            <div className="connect_wallet" onClick={handleConnectClick}>
               {/* <p>
                 {isHydrated && address
                   ? `${address.slice(0, 6)}...${address.slice(0, 6)}`
---
+++ app/routes/lore.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const lore = () => {
+  return <div>lore</div>;
+};
+
+export default lore;
