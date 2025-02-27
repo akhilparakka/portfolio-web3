17b1040d254b9669a45e75b45a7fa7f2815776f5
Akhil <akhil.parakka@blockstack.tech>
2025-02-27 23:10:28 +0530

    added vercel onfig

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     remix({
-      future: {},
+      future: {
+        v3_fetcherPersist: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: false,
+        v3_relativeSplatPath: false,
+        v3_throwAbortReason: false,
+      },
     }),
   ],
 });
